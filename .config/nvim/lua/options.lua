2126a5fdc95f2a8b51e4b0866110050da17cba9b
Samuel Oliveira <sasoliveira@gmail.com>
2022-08-24 21:21:26 +0100

    remake nvom config in lua

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,12 @@
+local o = vim.o
+
+o.termguicolors = true
+
+o.number = true
+o.cursorline = true
+o.expandtab = true
+o.cindent = true
+o.tabstop = 4
+o.clipboard = 'unnamedplus'
+o.ignorecase = true
+o.smartcase = true
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,40 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+end
+
+return require('packer').startup({
+    function(use)
+        use('wbthomason/packer.nvim')
+
+        use({
+            'doums/darcula',
+            config = function()
+                vim.cmd[[colorscheme darcula]]
+            end,
+        })
+        
+        use({ 
+            'feline-nvim/feline.nvim',
+            branch = '0.5-compat',
+            requires = 'kyazdani42/nvim-web-devicons',
+            config = function()
+                require('feline').setup()
+            end,
+        })
+
+        if packer_bootstrap then
+            require('packer').sync()
+        end
+
+    end,
+    config = {
+        display = {
+            open_fn = function()
+                return require('packer.util').float({ border = 'single' })
+            end
+        }
+    }
+})
