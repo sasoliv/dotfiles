23aa45ee5ec895124b56c64364a2ca90b57af0a7
Samuel Oliveira <sasoliveira@gmail.com>
2022-09-06 23:52:44 +0100

    redirect http to https

---
+++ tools/generate-certificate-issued-by-own-ca.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eu
+org=saso
+domain=pi
+
+openssl genpkey -algorithm RSA -out "$org".key
+openssl req -x509 -key "$org".key -out "$org".crt \
+    -subj "/CN=$org/O=$org"
+
+openssl genpkey -algorithm RSA -out "$domain".key
+openssl req -new -key "$domain".key -out "$domain".csr \
+    -subj "/CN=$domain/O=$org"
+
+openssl x509 -req -in "$domain".csr -days 365 -out "$domain".crt \
+    -CA "$org".crt -CAkey "$org".key -CAcreateserial \
+    -extfile <(cat <<END
+basicConstraints = CA:FALSE
+subjectKeyIdentifier = hash
+authorityKeyIdentifier = keyid,issuer
+subjectAltName = DNS:$domain
+END
+    )
+
