345f3f5b0b0cb5e8ebe2a669d3d10ab88f57e8bf
Samuel Oliveira <sasoliveira@gmail.com>
2022-10-30 08:16:54 +0000

    add delete feature to rofi file browser

--- .config/rofi/file-browser/file-browser.sh
+++ .config/rofi/file-browser/file-browser.sh
@@ -9,6 +9,9 @@ ACTION_UP="UP"
 ACTION_BACK="BACK"
 ACTION_OPEN="OPEN"
 ACTION_EDIT="EDIT"
+ACTION_DELETE="DELETE"
+ACTION_DELETE_CONFIRM="DELETE_CONFIRM"
+ACTION_DELETE_CANCEL="DELETE_CANCEL"
 
 PREV_PATH_FILE=$XDG_CONFIG_HOME
 if [ -z "$PREV_PATH_FILE" ]
@@ -40,8 +43,7 @@ browse() {
         echo "$CUR_DIR" > $PREV_PATH_FILE
 
     else
-        handleFile "$PREV_PATH$SELECTION"
-        exit 0
+        handleFile "$PREV_PATH$SELECTION"        
     fi
 }
 
@@ -52,13 +54,15 @@ up() {
 }
 
 handleFile() {
-    FILE=$1
-    FILE_ESCAPED=$(echo "$FILE" | sed 's/\//\\\//g')    
+    FILE="$1"
     echo -en "\0message\x1f${FILE}\n"
 
     echo -en " back\0info\x1f${ACTION_BACK};${FILE}\n"
     echo -en " open\0info\x1f${ACTION_OPEN};${FILE}\n"
     echo -en " edit\0info\x1f${ACTION_EDIT};${FILE}\n"
+    echo -en " delete\0info\x1f${ACTION_DELETE};${FILE}\n"
+
+    exit 0
 }
 
 back() {
@@ -75,6 +79,22 @@ edit() {
     exit 0
 }
 
+delete() {
+    echo -en "\0message\x1f ${PREV_PATH}\n"
+    echo -en " cancel\0info\x1f${ACTION_DELETE_CANCEL};${PREV_PATH}\n"
+    echo -en " ok\0info\x1f${ACTION_DELETE_CONFIRM};${PREV_PATH}\n"
+    exit 0
+}
+
+deleteCancel() {
+    handleFile "${PREV_PATH}"
+}
+
+deleteConfirm() {
+    rm "${PREV_PATH}"
+    exit 0
+}
+
 echo -en "\0prompt\x1f\n"
 echo -en "\0keep-selection\x1ffalse\n"
 
@@ -82,6 +102,9 @@ case "$ACTION" in
     "$ACTION_BACK") back ;;
     "$ACTION_OPEN") open ;;
     "$ACTION_EDIT") edit ;;
+    "$ACTION_DELETE") delete ;;
+    "$ACTION_DELETE_CANCEL") deleteCancel ;;
+    "$ACTION_DELETE_CONFIRM") deleteConfirm ;;
     "$ACTION_BROWSE") browse ;;
     "$ACTION_UP") up ;;
     *) init ;;
