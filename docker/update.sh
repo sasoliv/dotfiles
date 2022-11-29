c26b49c552a027583b3fde13c9bf94d6991cabaf
Samuel Oliveira <sasoliveira@gmail.com>
2022-11-29 21:19:03 +0000

    make docker services update script runnable from anywhere

--- docker/update.sh
+++ docker/update.sh
@@ -1,4 +1,10 @@
-docker-compose pull
-docker-compose up --force-recreate --build -d
+#!/usr/bin/env sh
+
+dockerCompose=$(readlink -f "$0")
+dockerCompose=$(dirname "$dockerCompose")
+dockerCompose="$dockerCompose/docker-compose.yaml"
+
+docker-compose -f $dockerCompose pull
+docker-compose -f $dockerCompose up --force-recreate --build -d
 docker image prune -f
 
