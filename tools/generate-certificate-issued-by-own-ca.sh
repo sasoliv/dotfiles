c845d06191f19308a42906a37c58429f05d8596b
Samuel Oliveira <sasoliveira@gmail.com>
2023-04-24 20:02:14 +0100

    pi to pluto
    adapt docker tools and system level backups

--- tools/generate-certificate-issued-by-own-ca.sh
+++ tools/generate-certificate-issued-by-own-ca.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -eu
 org=saso
-domain=pi
+domain=pluto
 
+# Generate CA
 openssl genpkey -algorithm RSA -out "$org".key
 openssl req -x509 -key "$org".key -days 3650 -out "$org".crt \
     -subj "/CN=$org/O=$org"
 
+# Generate domain
 openssl genpkey -algorithm RSA -out "$domain".key
 openssl req -new -key "$domain".key -out "$domain".csr \
     -subj "/CN=$domain/O=$org"
