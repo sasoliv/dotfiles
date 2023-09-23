a0ea6c9a98a6304004dc75459c0e5df4c451030d
Samuel Oliveira <sasoliveira@gmail.com>
2023-09-23 17:36:05 +0100

    add sonarr to the stack

--- docker/update.sh
+++ docker/update.sh
@@ -4,7 +4,7 @@ dockerCompose=$(readlink -f "$0")
 dockerCompose=$(dirname "$dockerCompose")
 dockerCompose="$dockerCompose/docker-compose.yaml"
 
-docker compose -f $dockerCompose pull
-docker compose -f $dockerCompose up --force-recreate --build --remove-orphans -d
+docker compose -f $dockerCompose pull $@
+docker compose -f $dockerCompose up --force-recreate --build --remove-orphans -d $@
 docker image prune -f
 
