aa63a1d74ddce5f897e049d4bd1e6ac4547c5091
Samuel Oliveira <sasoliveira@gmail.com>
2025-11-29 18:58:27 +0000

    add poe2 filter copy script

--- sources/updateNetSinkFilterAndCopyToPoe2.sh
+++ sources/updateNetSinkFilterAndCopyToPoe2.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-poeFiltersFolder="/run/media/saso/M2/SteamLibrary/steamapps/compatdata/238960/pfx/drive_c/users/steamuser/My Documents/My Games/Path of Exile/"
+poeFiltersFolder="/run/media/saso/M2/SteamLibrary/steamapps/compatdata/2694490/pfx/drive_c/users/steamuser/My Documents/My Games/Path of Exile 2/"
 refFolder=$(pwd)
-gitFolder="$refFolder/NeverSink-Filter"
+gitFolder="$refFolder/NeverSink-Filter-for-PoE2"
 
 cd "$gitFolder"
 echo "pulling..."
@@ -25,7 +25,7 @@ echo ""
 echo "##########################################################################################"
 echo "copying..."
 cd "$gitFolder"
-cp *.filter /run/media/saso/M2/SteamLibrary/steamapps/compatdata/238960/pfx/drive_c/users/steamuser/My\ Documents/My\ Games/Path\ of\ Exile/
+cp *.filter /run/media/saso/M2/SteamLibrary/steamapps/compatdata/2694490/pfx/drive_c/users/steamuser/My\ Documents/My\ Games/Path\ of\ Exile\ 2/
 echo ""
 
 echo "##########################################################################################"
